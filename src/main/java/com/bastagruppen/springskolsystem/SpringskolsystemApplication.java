d308e058deab8ed3c7b5396cfdbd59f3fb2c4d12
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-11-10 11:39:08 +0100

    initial commit

---
+++ src/main/java/com/bastagruppen/springskolsystem/SpringskolsystemApplication.java
@@ -0,0 +1,13 @@
+package com.bastagruppen.springskolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringskolsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringskolsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastagruppen/springskolsystem/SpringskolsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastagruppen.springskolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringskolsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
